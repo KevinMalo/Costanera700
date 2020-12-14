bf0cd8460af2b4b1e6ad68ed061ac1616e362428
Kevin Bueno <kean.solano@gmail.com>
2020-12-13 22:21:51 -0500

    post method /uploads

--- frontend/src/views/Uploads.vue
+++ frontend/src/views/Uploads.vue
@@ -1,63 +1,64 @@
 <template>
-  <v-card color="basil">
-    <v-card-title class="text-center justify-center py-6">
-      <h1 class="font-weight-bold display-3 basil--text">
-        UPLOADS
-      </h1>
-    </v-card-title>
+<v-container>
+<v-form method="post" :action="`${url}${date}`">
 
-    <v-tabs
-      v-model="tab"
-      background-color="transparent"
-      color="basil"
-      grow
-    >
-      <v-tab
-        v-for="item in items"
-        :key="item"
-      >
-        {{ item }}
-      </v-tab>
-    </v-tabs>
-
-    <v-tabs-items v-model="tab">
-      <v-tab-item
-        v-for="item in items"
-        :key="item"
-      >
+  <v-col cols="6">
+   <v-container class="grey lighten-5">
+    <v-row no-gutters>
+      <v-col>
         <v-card
-          color="basil"
-          flat
-        >
-          <v-card-text>{{ text }}</v-card-text>
+          class="pa-2"
+          outlined
+          tile> 
+          
+          <v-text-field
+            label="Date" v-model="date"
+          ></v-text-field>
         </v-card>
-      </v-tab-item>
-    </v-tabs-items>
-  </v-card>
-</template>
+          <v-btn
+            color="primary"
+            text
+            type="submit"
+          >
+            Submit
+          </v-btn>
+      </v-col>
+      <v-col order="12">
+        <v-card
+          class="pa-2"
+          outlined
+          tile
+        >
+      <v-date-picker
+      v-model="picker"
+      color="green lighten-1"
+    ></v-date-picker>
+        </v-card>
+      </v-col>
+    </v-row>
+  </v-container>
+  
+  </v-col>
+
+
+
+</v-form>
+</v-container>
+</template>  
 
 <script>
-
-export default {
-  name: "Uploads",
-   data () {
+  export default {
+    name: "Uploads",
+    data () {
       return {
-        tab: null,
-        items: [
-          'Products', 'Buyers', 'Transacts'
-        ],
-        text: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.',
+        picker: new Date().toISOString().substr(0, 10),
+        picker2: "",
+        date: Math.floor(Date.now() / 1000),
+        url: "http://localhost:9000/uploads/"
       }
     },
-};
+  }
 </script>
 
 <style scope>
-/* Helper classes */
-.basil {
-  background-color: #FFFBE6 !important;
-}
-.basil--text {
-  color: #356859 !important;
-}
 </style>
