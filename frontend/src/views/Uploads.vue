47913776bb7362e82f31b3cbc9556c8356c535c4
Kevin Bueno <kean.solano@gmail.com>
2020-12-30 09:17:13 -0500

    uploads

--- frontend/src/views/Uploads.vue
+++ frontend/src/views/Uploads.vue
@@ -1,63 +1,54 @@
 <template>
-<v-container>
-<v-form method="post" :action="`${url}${date}`">
+  <v-container>
+    <v-form method="post" :action="`${url}${date}`">
+      <v-col cols="6">
+        <v-container class="grey lighten-5">
+          <v-row no-gutters>
+            <v-col>
+              <v-card class="pa-2" outlined tile>
+                <v-text-field label="Date" v-model="date"></v-text-field>
+              </v-card>
 
-  <v-col cols="6">
-   <v-container class="grey lighten-5">
-    <v-row no-gutters>
-      <v-col>
-        <v-card
-          class="pa-2"
-          outlined
-          tile> 
-          
-          <v-text-field
-            label="Date" v-model="date"
-          ></v-text-field>
-        </v-card>
-          <v-btn
-            color="primary"
-            text
-            type="submit"
-          >
-            Submit
-          </v-btn>
+              <v-layout row wrap>
+                <!-- other code -->
+                <v-text-field
+                  label="Date"
+                  readonly
+                  :value="fromDateDisp"
+                ></v-text-field>
+              </v-layout>
+
+              <v-date-picker
+                locale="en-in"
+                v-model="fromDateVal"
+                no-title
+              ></v-date-picker>
+
+              <v-btn color="primary" text type="submit"> Submit </v-btn>
+            </v-col>
+          </v-row>
+        </v-container>
       </v-col>
-      <v-col order="12">
-        <v-card
-          class="pa-2"
-          outlined
-          tile
-        >
-      <v-date-picker
-      v-model="picker"
-      color="green lighten-1"
-    ></v-date-picker>
-        </v-card>
-      </v-col>
-    </v-row>
+    </v-form>
   </v-container>
-  
-  </v-col>
-
-
-
-</v-form>
-</v-container>
 </template>  
 
 <script>
-  export default {
-    name: "Uploads",
-    data () {
-      return {
-        picker: new Date().toISOString().substr(0, 10),
-        picker2: "",
-        date: Math.floor(Date.now() / 1000),
-        url: "http://localhost:9000/uploads/"
-      }
+export default {
+  name: "Uploads",
+  data() {
+    return {
+      fromDateVal: null,
+      date: Math.floor(Date.now() / 1000),
+      url: "http://localhost:9000/uploads/",
+    };
+  },
+  computed: {
+      fromDateDisp() {
+        return this.fromDateVal;
+      },
     },
-  }
+};
 </script>
 
 <style scope>
